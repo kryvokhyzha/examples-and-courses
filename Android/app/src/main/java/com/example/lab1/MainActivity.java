16b49b30cee1057b376f0fed2a1aa2bde3c77818
kryvokhyzha <kriwohizha@gmail.com>
2020-02-16 23:27:48 +0200

    Edited Lab1

--- Android/app/src/main/java/com/example/lab1/MainActivity.java
+++ Android/app/src/main/java/com/example/lab1/MainActivity.java
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
