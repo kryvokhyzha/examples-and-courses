19cc563cda69abee2e4ceaf66404596004d84cca
kryvokhyzha <kriwohizha@gmail.com>
2020-02-24 21:27:39 +0200

    Added Android - Lab2

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner groupSpinner, facultiesSpinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String[] groups = getResources().getStringArray(R.array.groups);
+        final String[] faculties = getResources().getStringArray(R.array.faculties);
+
+        groupSpinner = findViewById(R.id.groups);
+        facultiesSpinner = findViewById(R.id.faculties);
+
+        ArrayAdapter<String> groupAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, groups);
+        groupAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> facultyAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, faculties);
+        facultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        groupSpinner.setAdapter(groupAdapter);
+        facultiesSpinner.setAdapter(facultyAdapter);
+    }
+
+    public void showMsg(View view) {
+
+        String group = groupSpinner.getSelectedItem().toString();
+        String faculty = facultiesSpinner.getSelectedItem().toString();
+
+        if (group.equals("") || faculty.equals("")) {
+            String msg = "Заповніть всі поля!";
+            Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            String msg = "Факультет: " + faculty + '\n' + "Група: " + group;
+            Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public void setDefault(View view) {
+        groupSpinner.setSelection(0);
+        facultiesSpinner.setSelection(0);
+    }
+}
