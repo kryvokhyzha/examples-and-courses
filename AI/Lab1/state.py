04cf545fce19789ee1da1fe74375709af9c638d1
kryvokhyzha <kriwohizha@gmail.com>
2020-02-27 00:34:04 +0200

    Edited AI - Lab1

--- AI/Lab1/state.py
+++ AI/Lab1/state.py
@@ -1,9 +1,7 @@
 class State:
-    # array with the position of the people in the order w1, w2,... h1, h2, ... (0 for initial shore, 1 for goal shore)
     shore = None
     boat = 0
     depth = 0
-    # array of States that lead to the current State
     path = None
 
     def __init__(self, shore=None, boat=0):
