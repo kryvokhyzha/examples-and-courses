e4e4dea751091494c14c1f0dd9346c52e5e05973
kryvokhyzha <kriwohizha@gmail.com>
2020-04-22 21:40:56 +0300

    Edited AI-Lab4

--- AI/Lab4/task.pl
+++ AI/Lab4/task.pl
@@ -1,10 +1,10 @@
 :- dynamic jars/2.
-solve(jar(VolumeA, A), jar(VolumeB, B), target(Target), Actions):-
-  retractall(jars(_, _)),
-  bfs(volumes(VolumeA, VolumeB), target(Target), [state(jars(A, B), actions([init]))], Actions).
+solve(jar(VolumeA, A), jar(VolumeB, B), jar(VolumeC, C), target(Target), Actions):-
+  retractall(jars(_, _, _)),
+  bfs(volumes(VolumeA, VolumeB, VolumeC), target(Target), [state(jars(A, B, C), actions([init]))], Actions).
   
-bfs(_Volumes, target(Target), [state(jars(A, B), Actions)|_Other], Actions):-
-  (A = Target; B = Target).
+bfs(_Volumes, target(Target), [state(jars(A, B, C), Actions)|_Other], Actions):-
+  ((A = Target, B = Target); (A = Target, C = Target); (B = Target, C = Target)).
 bfs(Volumes, target(Target), [state(Jars, HeadActions)|Buffer], Actions):-
   generate_states(Volumes, Jars, HeadActions, NextStates),
   append(Buffer, NextStates, NextBuffer),
@@ -17,31 +17,45 @@ generate_states(Volumes, Jars, actions(PrevActions), NextStates):-
           
 next_unique_state(Volumes, Jars, PrevActions, NextState):-
   next_state(Volumes, Jars, PrevActions, NextState),
-  NextState = state(jars(A, B), _Actions),
-  \+ jars(A, B),
-  assert(jars(A, B)).
+  NextState = state(jars(A, B, C), _Actions),
+  \+ jars(A, B, C),
+  assert(jars(A, B, C)).
   
-next_state(volumes(VolumeA, VolumeB), jars(A, B), actions(PrevActions), NextState):-
-  \+ B = 0, % из B можно перелить в A 
+next_state(volumes(VolumeA, VolumeB, VolumeC), jars(A, B, C), actions(PrevActions), NextState):-
+  ((\+ B = 0), (\+ A = VolumeA)) , % из B можно перелить в A 
   APlusB is min((A + B), VolumeA), % A = A + B
   Delta is APlusB - A, % сколько смогли перелить
   BMinusDelta is B - Delta, % B = B - Delta
-  NextState = state(jars(APlusB, BMinusDelta), actions([из_B_в_A|PrevActions]));
+  NextState = state(jars(APlusB, BMinusDelta, C), actions([из_B_в_A|PrevActions]));
   
-  \+ B = 0, % из B можно вылить
-  NextState = state(jars(A, 0), actions([из_В_в_бочку|PrevActions]));
+  ((\+ B = 0), (\+ C = VolumeC)), % из B можно перелить в C
+  CPlusB is min((C + B), VolumeC), % C = C + B
+  Delta is CPlusB - C, % сколько смогли перелить
+  BMinusDelta is B - Delta, % B = B - Delta
+  NextState = state(jars(A, BMinusDelta, CPlusB), actions([из_B_в_C|PrevActions]));
   
-  \+ A = 0, % из A можно перелить в B
-  BPlusA is min((A + B), VolumeB), % A = A + B
+  ((\+ A = 0), (\+ B = VolumeB)), % из A можно перелить в B
+  BPlusA is min((A + B), VolumeB), % B = B + A
   Delta is BPlusA - B, % сколько смогли перелить
   AMinusDelta is A - Delta, 
-  NextState = state(jars(AMinusDelta, BPlusA), actions([из_А_в_В|PrevActions]));
+  NextState = state(jars(AMinusDelta, BPlusA, C), actions([из_А_в_В|PrevActions]));
+
+  ((\+ A = 0), (\+ C = VolumeC)), % из A можно перелить в C
+  CPlusA is min((A + C), VolumeC), % C = C + A
+  Delta is CPlusA - C, % сколько смогли перелить
+  AMinusDelta is A - Delta, 
+  NextState = state(jars(AMinusDelta, B, CPlusA), actions([из_А_в_C|PrevActions]));
+
+  ((\+ C = 0), (\+ B = VolumeB)), % из C можно перелить в B
+  BPlusC is min((C + B), VolumeB), % B = B + C
+  Delta is BPlusC - B, % сколько смогли перелить
+  CMinusDelta is C - Delta, 
+  NextState = state(jars(A, BPlusC, CMinusDelta), actions([из_C_в_В|PrevActions]));
+
+  ((\+ C = 0), (\+ A = VolumeA)), % из C можно перелить в A 
+  APlusC is min((A + C), VolumeA), % A = A + C
+  Delta is APlusC - A, % сколько смогли перелить
+  CMinusDelta is C - Delta, % C = C - Delta
+  NextState = state(jars(APlusC, B, CMinusDelta), actions([из_C_в_A|PrevActions])).
   
-  \+ A = 0, % из A можно вылить
-  NextState = state(jars(0, B), actions([из_А_в_бочку|PrevActions]));
-  
-  \+ B = VolumeB, % в B можно зачерпнуть
-  NextState = state(jars(A, VolumeB), actions([налить_в_В|PrevActions]));
-  
-  \+ A = VolumeA, % в A можно зачерпнуть
-  NextState = state(jars(VolumeA, B), actions([налить_в_А|PrevActions])).
+
