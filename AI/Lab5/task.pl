5ee1bc752d5e8674e5c4a821445e09efc8ca0164
kryvokhyzha <kriwohizha@gmail.com>
2020-05-07 02:00:20 +0300

    Edited AI-Lab5

--- AI/Lab5/task.pl
+++ AI/Lab5/task.pl
@@ -1,24 +1,17 @@
-% Figure 12.3  A best-first search program.
 
 
-% bestfirst(Start, Solution): Solution is a path from Start to a goal
 
 bestfirst( Start, Solution) :-
   expand( [], l( Start, 0/0),  9999, _, yes, Solution).
 	%  Assume 9999 is greater than any f-value
 
-% expand( Path, Tree, Bound, Tree1, Solved, Solution):
-%   Path is path between start node of search and subtree Tree,
-%   Tree1 is Tree expanded within Bound,
-%   if goal found then Solution is solution path and Solved = yes
-
-%  Case 1: goal leaf-node, construct a solution path
+% --------- 1
 
 expand( P, l( N, _), _, _, yes, [N|P])  :-
    goal(N).
 
-%  Case 2: leaf-node, f-value less than Bound
-%  Generate successors and expand them within Bound.
+
+% --------- 2
 
 expand( P, l(N,F/G), Bound, Tree1, Solved, Sol)  :-
   F  =<  Bound,
@@ -31,9 +24,7 @@ expand( P, l(N,F/G), Bound, Tree1, Solved, Sol)  :-
      Solved = never                        % N has no successors - dead end
   ) .
 
-%  Case 3: non-leaf, f-value less than Bound
-%  Expand the most promising subtree; depending on 
-%  results, procedure continue will decide how to proceed
+% --------- 3
 
 expand( P, t(N,F/G,[T|Ts]), Bound, Tree1, Solved, Sol)  :-
   F  =<  Bound,
@@ -41,13 +32,11 @@ expand( P, t(N,F/G,[T|Ts]), Bound, Tree1, Solved, Sol)  :-
   expand( [N|P], T, Bound1, T1, Solved1, Sol),
   continue( P, t(N,F/G,[T1|Ts]), Bound, Tree1, Solved1, Solved, Sol).
 
-%  Case 4: non-leaf with empty subtrees
-%  This is a dead end which will never be solved
+% --------- 4
 
 expand( _, t(_,_,[]), _, _, never, _) :- !.
 
-%  Case 5:  f-value greater than Bound
-%  Tree may not grow.
+% --------- 5
 
 expand( _, Tree, Bound, Tree, no, _)  :-
   f( Tree, F), F > Bound.
@@ -65,8 +54,10 @@ continue( P, t(N,F/G,[_|Ts]), Bound, Tree1, never, Solved, Sol)  :-
   bestf( Ts, F1),
   expand( P, t(N,F1/G,Ts), Bound, Tree1, Solved, Sol).
 
+
+
+% --------- 6 
 % succlist( G0, [ Node1/Cost1, ...], [ l(BestNode,BestF/G), ...]):
-%   make list of search leaves ordered by their F-values
 
 succlist( _, [], []).
 
@@ -77,7 +68,7 @@ succlist( G0, [N/C | NCs], Ts)  :-
   succlist( G0, NCs, Ts1),
   insert( l(N,F/G), Ts1, Ts).
 
-% Insert T into list of trees Ts preserving order w.r.t. f-values
+% --------- 7
 
 insert( T, Ts, [T | Ts])  :-
   f( T, F), bestf( Ts, F1),
@@ -105,16 +96,17 @@ min( X, Y, Y).
 
 
 
-s( [Empty | Tiles], [Tile | Tiles1], 1)  :-  % All arc costs are 1
-  swap( Empty, Tile, Tiles, Tiles1).         % Swap Empty and Tile in Tiles 
+s([Empty | Tiles], [Tile | Tiles1], 1):-  % Стоимости всех дуг равны 1
+  swap(Empty, Tile, Tiles, Tiles1).         % Поменять местами пустую фишку 
+					% Empty и фишку Tile в списке Tiles
 
-swap( Empty, Tile, [Tile | Ts], [Empty | Ts] )  :-
-  mandist( Empty, Tile, 1).                  % Manhattan distance = 1
+swap(Empty, Tile, [Tile | Ts], [Empty | Ts] ):-
+  mandist( Empty, Tile, 1).                  % Манхэттенское расстояние равно 1
 
 swap( Empty, Tile, [T1 | Ts], [T1 | Ts1] )  :-
   swap( Empty, Tile, Ts, Ts1).
 
-mandist( X/Y, X1/Y1, D)  :-          % D is Manhhattan dist. between two squares
+mandist( X/Y, X1/Y1, D)  :-          % D - это манхэттенское расстояние между двумя клетками
   dif( X, X1, Dx),
   dif( Y, Y1, Dy),
   D is Dx + Dy.
@@ -127,8 +119,8 @@ dif( A, B, D)  :-              % D is |A-B|
 
 h( [Empty | Tiles], H)  :-
   goal( [Empty1 | GoalSquares] ),
-  totdist( Tiles, GoalSquares, D),      % Total distance from home squares
-  seq( Tiles, S),                       % Sequence score
+  totdist( Tiles, GoalSquares, D),      % Суммарное расстояние от исходных клеток
+  seq( Tiles, S),                       % Оценка упорядоченности
   H is D + 3*S.
 
 totdist( [], [], 0).
@@ -138,7 +130,7 @@ totdist( [Tile | Tiles], [Square | Squares], D)  :-
   totdist( Tiles, Squares, D2),
   D is D1 + D2.
 
-% seq( TilePositions, Score): sequence score
+% seq( TilePositions, Score): оценка упорядоченности
 
 seq( [First | OtherTiles], S)  :-
   seq( [First | OtherTiles ], First, S).
@@ -151,9 +143,9 @@ seq( [Tile1, Tile2 | Tiles], First, S)  :-
 seq( [Last], First, S)  :-
   score( Last, First, S).
 
-score( 2/2, _, 1)  :-  !.              % Tile in centre scores 1
+score( 2/2, _, 1)  :-  !.              % ценка фишки, стоящей в центре, равна 1
 
-score( 1/3, 2/3, 0)  :-  !.            % Proper successor scores 0
+score( 1/3, 2/3, 0)  :-  !.            % Оценка фишки, за которой следует допустимый преемник, равна 0
 score( 2/3, 3/3, 0)  :-  !.
 score( 3/3, 3/2, 0)  :-  !.
 score( 3/2, 3/1, 0)  :-  !.
@@ -162,40 +154,11 @@ score( 2/1, 1/1, 0)  :-  !.
 score( 1/1, 1/2, 0)  :-  !.
 score( 1/2, 1/3, 0)  :-  !.
 
-score( _, _, 2).                       % Tiles out of sequence score 2
+score( _, _, 2).                       % Оценка фишкк, за которой следует недопустимый преемник, равна 2
 
 goal( [2/2,1/3,2/3,3/3,3/2,3/1,2/1,1/1,1/2] ).  % Goal squares for tiles
 
-% Display a solution path as a list of board positions
 
-showsol( [] ).
-
-showsol( [P | L] )  :-
-  showsol( L),
-  nl, write( '---'),
-  showpos( P).
-
-% Display a board position
-
-showpos( [S0,S1,S2,S3,S4,S5,S6,S7,S8] )  :-
-  member( Y, [3,2,1] ),                           % Order of Y-coordinates
-  nl, member( X, [1,2,3] ),                       % Order of X-coordinates
-  member( Tile-X/Y,                               % Tile on square X/Y
-          [' '-S0,1-S1,2-S2,3-S3,4-S4,5-S5,6-S6,7-S7,8-S8] ),
-  write( Tile),
-  fail                                            % Backtrack to next square
-  ;
-  true.                                           % All squares done
-
-% Starting positions for some puzzles
-
-start1( [2/2,1/3,3/2,2/3,3/3,3/1,2/1,1/1,1/2] ).  % Requires 4 steps
-
-start2( [2/1,1/2,1/3,3/3,3/2,3/1,2/2,1/1,2/3] ).  % Requires 5 steps
-
-start3( [2/2,2/3,1/3,3/1,1/2,2/1,3/3,1/1,3/2] ).  % Requires 18 steps
-
-% An example query: ?- start1( Pos), bestfirst( Pos, Sol), showsol( Sol).
 
 
 
