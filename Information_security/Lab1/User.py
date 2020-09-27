18af9057c1e200f90e1d4d6f2cc6c71631149129
kryvokhyzha <kriwohizha@gmail.com>
2020-09-27 21:45:22 +0300

    Information secirity lab1

---
+++ Information_security/Lab1/User.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
---
+++ Information_security/Lab1/main.py
@@ -0,0 +1,9 @@
+from utils import print_general_menu
+
+
+def main():
+    print_general_menu()
+
+
+if __name__ == '__main__':
+    main()
