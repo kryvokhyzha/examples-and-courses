93afc8d0b83e860b16b4a53d6deae031c6574e1c
kryvokhyzha <kriwohizha@gmail.com>
2021-04-13 00:54:18 +0300

    Add temp files for flat-wave-animation-project

--- Trash/lab_2/lab_2/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
-    current_path = os.getcwd()
-
-    WORKING_FOLDER = current_path + '/app/'
--- Trash/lab_2/lab_2/notebooks/export_modules.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-import warnings
-
-warnings.filterwarnings('ignore')
-sys.path.append(os.path.abspath(os.pardir))
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../..")))
